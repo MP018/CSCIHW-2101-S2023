73d8683b2de4d9d24a2d9aaf8f97b01404edfe9d
MIKAEL <pamuspum@go.stockton.edu>
2023-03-02 09:23:38 -0500

    Pyramid and Adlibs

--- HW1/Main.java
+++ HW1/Main.java
@@ -4,10 +4,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Start of Main");
         // Create an instance of the Pyramid class and call the makePyramid method (see Pyramid.java for more info)
+        Pyramid.Output();
 
         // Create an instance of the Adlibs class and call the story method (see Adlibs.java for more info)
+        Adlibs.story("James", 22, "Red");
 
-        // Create an instance of the Replace class and call the charReplace method (see Replace.java for more info)
+        // Create an instance of the Replace class and call the c "harReplace method (see Replace.java for more info)
 
     }
 }
