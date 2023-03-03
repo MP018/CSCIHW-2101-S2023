8e94c8b9e9965087793e2bdf3e6a4d62435b6f2b
MIKAEL <pamuspum@go.stockton.edu>
2023-03-02 21:49:04 -0500

    edit

--- HW1/Main.java
+++ HW1/Main.java
@@ -7,7 +7,7 @@ public class Main {
         Pyramid.Output();
 
         // Create an instance of the Adlibs class and call the story method (see Adlibs.java for more info)
-        Adlibs.story("Rogue the dog", 3, "brown");
+        Adlibs.story("Rogue, the dog,", 3, "brown");
 
         // Create an instance of the Replace class and call the c "harReplace method (see Replace.java for more info)
         Replace.charReplace("I MAKE BREAD", 2, 'T');
