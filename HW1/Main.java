f0e8c6f0d4ddc9bbe57ef8de4f00421279f3379e
MIKAEL <pamuspum@go.stockton.edu>
2023-03-02 21:45:53 -0500

    Homework

--- HW1/Main.java
+++ HW1/Main.java
@@ -3,11 +3,12 @@ package HW1;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Start of Main");
+        
         // Create an instance of the Pyramid class and call the makePyramid method (see Pyramid.java for more info)
         Pyramid.Output();
 
         // Create an instance of the Adlibs class and call the story method (see Adlibs.java for more info)
-        Adlibs.story("Mikael", 18, "black");
+        Adlibs.story("Rogue the dog", 3, "brown");
 
         // Create an instance of the Replace class and call the c "harReplace method (see Replace.java for more info)
         Replace.charReplace("I MAKE BREAD", 2, 'T');
--- HW1/Replace.java
+++ HW1/Replace.java
@@ -13,10 +13,6 @@ public class Replace {
         System.out.println(modifiedS);
         return null;
         
-
-
-        //return null;
-        
     }
     
 }
